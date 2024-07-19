67d09a78e970d5c6207e9391198e045d181463bc
Ahmad <131032658+MuhammadAhmadSaaim@users.noreply.github.com>
2024-07-19 09:19:22 +0500

    fixed layout syntax

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,53 +5,51 @@ import { ColorPalette } from "@/constants/Colors";
 import icons from "@/constants/Icons";
 const TabsLayout = () => {
 
-    return (
-        <Tabs
-            screenOptions={{
-                tabBarActiveTintColor: ColorPalette.primary,
-                tabBarInactiveTintColor: ColorPalette.text,
-                tabBarShowLabel: true,
-                tabBarLabelStyle: {
-                    fontSize: 10,
-                    position: 'relative',
-                    top: -12,
-                },
-                tabBarStyle: {
-                    backgroundColor: ColorPalette.greyNav,
-                    borderTopWidth: 0,
-                    height: 70,
-                    borderRadius: 10,
-                    overflow: 'hidden',
-                    position: 'absolute',
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    elevation: 5, // Android
-                    shadowColor: '#000', // iOS
-                    shadowOffset: { width: 0, height: 2 }, // iOS
-                    shadowOpacity: 0.25, // iOS
-                    shadowRadius: 3.5, // iOS
-                    margin: 16,
-                },
-            }}
-        >
-            <Tabs.Screen
-                name="(home)"
-                options={{
-                    headerShown: false,
-                    tabBarLabel: 'Dashboard',
-                    tabBarIcon: ({ color, focused }) => (
-                        <Image
-                            source={focused ? icons.home : icons.homeYellow}
-                            style={{ width: 24, height: 24, tintColor: color }}
-                        />
-                    ),
-                }}
-
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: ColorPalette.primary,
+        tabBarInactiveTintColor: ColorPalette.text,
+        tabBarShowLabel: true,
+        tabBarLabelStyle: {
+          fontSize: 10,
+          position: 'relative',
+          top: -12,
+        },
+        tabBarStyle: {
+          backgroundColor: ColorPalette.greyNav,
+          borderTopWidth: 0,
+          height: 70,
+          borderRadius: 10,
+          overflow: 'hidden',
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 0,
+          elevation: 5, // Android
+          shadowColor: '#000', // iOS
+          shadowOffset: { width: 0, height: 2 }, // iOS
+          shadowOpacity: 0.25, // iOS
+          shadowRadius: 3.5, // iOS
+          margin: 16,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="(home)"
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Dashboard',
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={focused ? icons.home : icons.homeYellow}
+              style={{ width: 24, height: 24, tintColor: color }}
             />
           ),
         }}
+
       />
+
       <Tabs.Screen
         name="subscribtion"
         options={{
@@ -62,25 +60,25 @@ const TabsLayout = () => {
               source={focused ? icons.send : icons.sendYellow}
               style={{ width: 24, height: 24, tintColor: color }}
             />
+          ),
+        }}
+      />
 
-            <Tabs.Screen
-                name="(profile)"
-                options={{
-                    headerShown: false,
+      <Tabs.Screen
+        name="(profile)"
+        options={{
+          headerShown: false,
 
-                    tabBarLabel: 'Portfolio',
+          tabBarLabel: 'Portfolio',
 
-                    tabBarIcon: ({ color, focused }) => (
-                        <Image
-                            source={focused ? icons.user : icons.userYellow}
-                            style={{ width: 24, height: 24, tintColor: color }}
-                        />
-                    ),
-                }}
-
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={focused ? icons.user : icons.userYellow}
+              style={{ width: 24, height: 24, tintColor: color }}
             />
           ),
         }}
+
       />
     </Tabs>
   );
