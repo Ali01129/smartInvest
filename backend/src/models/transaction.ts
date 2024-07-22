b864439d3bd78a3e02eb25107ce2e189e114267c
Ali Nawaz <131615374+Ali01129@users.noreply.github.com>
2024-07-22 12:55:59 +0500

    transaction

---
+++ backend/src/models/transaction.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface ITransaction extends Document {
+    senderId: string;
+    receiverId: string;
+    amount: number;
+    type: string;
+}
+
+const transactionSchema = new Schema<ITransaction>({
+    senderId: { type: String, required: true },
+    receiverId: { type: String, required: true },
+    amount: { type: Number, required: true },
+    type: { type: String, required: true },
+}, { timestamps: true });
+
+export default mongoose.model<ITransaction>('Transaction', transactionSchema);
