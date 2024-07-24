13f7d529010e028fd005b8323222a69c9b748090
Ahmad <131032658+MuhammadAhmadSaaim@users.noreply.github.com>
2024-07-24 11:34:50 +0500

    added forget pass api

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -4,8 +4,8 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  referralCode: { type: String,required: false},
-  phantomWalletAddress: { type: String, unique: true ,required: false},
+  referralCode: { type: String, required: false },
+  phantomWalletAddress: { type: String, unique: true, required: false },
 });
 
 const User = mongoose.model('User', UserSchema);
